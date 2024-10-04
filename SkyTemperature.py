c52e79dd2e258468e21b3c2fb61b14c085fe19ea
Priyesh <119525719+priyeshppuluckul@users.noreply.github.com>
2024-10-04 22:50:05 +0200

    Corrected typos

--- SkyTemperature.py
+++ SkyTemperature.py
@@ -1,9 +1,9 @@
 ## @package SkyTemperature
 # This package calculates the sky temperature.
-# the Clark-Allen model is used to calculate the  clear sky emissivity.
-# Simulations tools like EnergyPlus uses this model to calculate sky emissivity and temperature.
-# The Clear sky model is then converted for cloudy sky using a cloud correlation factor.
-# There are two methods given: one uses the generic relation between sky emissivity and the ambient temperature
+# The Clark-Allen model is used to calculate the  clear sky emissivity.
+# Simulations tools like EnergyPlus use this model to calculate sky emissivity and temperature.
+# The Clear sky model is then converted to a cloudy sky using a cloud correlation factor.
+# Two methods are given: one uses the generic relation between sky emissivity and the ambient temperature
 # and the other uses the energy plus calculation model.
 
 import numpy as np
@@ -13,19 +13,19 @@ class SkyTemperature:
         # Stefan-Boltzmann constant
         self.sigma = 5.670374419e-8
 
-    # Calculates sky emissivity using Clark-Allen model
+    # Calculates sky emissivity using the Clark-Allen model
     def emissivity(self, N,  t_dp):
         t_dp += 273.15
         # Calculate sky emissivity
-        sky_emissivity = ( 0.787 + 0.767 * np.log(t_dp / 273) ) + ( (0.0224 * N) - (0.0035 * N ** 2) + (0.00028 * N ** 3))
+        sky_emissivity = ( 0.787 + 0.767 * np.log(t_dp / 273) ) + ( 1+ (0.0224 * N) - (0.0035 * N ** 2) + (0.00028 * N ** 3))
 
         return (sky_emissivity)
 
     ## Calculates sky temperature using the relation between emissivity and ambient temp
     # @param N Opaque sky cover in   tenths (0 - no cover, 10 - completely covered)
-    # @param t_air air temperature in degree Celsius
-    # @param t_dp dew point temperature in degree Celsius
-    # @return  sky temperature in degree Celsius
+    # @param t_air air temperature in degrees Celsius
+    # @param t_dp dew point temperature in degrees Celsius
+    # @return  sky temperature in degrees Celsius
     def get_temperature1(self, N,  t_air, t_dp):
         # Get sky emissivity
         sky_emissivity = self.emissivity(N, t_dp)
@@ -36,9 +36,9 @@ class SkyTemperature:
 
     ## Calculates sky temperature using EnergyPlus method
     # @param N Opaque sky cover in   tenths (0- no cover, 10- completely covered)
-    # @param t_air air temperature in degree Celsius
+    # @param t_air air temperature in degrees Celsius
     # @param t_dp dew point temperature in degree Celsius (-25°C + 25°C)
-    # @return  sky temperature in degree Celsius
+    # @return  sky temperature in degrees Celsius
     def get_temperature2(self, N,  t_air, t_dp):
         t_air += 273.15
 
